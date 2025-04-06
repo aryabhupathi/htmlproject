3a746354b6fbc7d9e482e81a52ba06c153e5c9e3
Arya Bhupathi <bhupathikonduru@gmail.com>
2025-04-07 00:04:53 +0530

    final

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,13 +1,2 @@
-function addToCart() {
-  let flavor = document.querySelector('input[name="flavor"]:checked').value;
-  let subscription = document.querySelector(
-    'input[name="subscription"]:checked'
-  ).value;
-  let notification = document.getElementById("notification");
-  notification.innerText = `Added to Cart: ${flavor} - ${subscription}`;
-  notification.style.display = "block";
-  setTimeout(() => {
-    notification.style.display = "none";
-  }, 3000);
-}
+
 
